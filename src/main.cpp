9dc0d2fe667e71329fe012a0c3deb10972341241
Luca Rubin <luca86r@gmail.com>
2024-09-07 10:50:45 +0200

    Load ELMdata and animition in async task

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,10 @@ ELM327Manager ELM327Manager;
 // === Display ===
 DisplayManager displayManager;
 
+// === Tasks ===
+TaskHandle_t taskLoadingAnimation = NULL;
+TaskHandle_t taskReadDataFromELM327 = NULL;
+
 void oledPrintData() {
 
   String lines = "";
@@ -57,10 +61,83 @@ void oledPrintData() {
   displayManager.printText(lines);
 }
 
-void readAndPrintAllData() {
+void taskReadDataFromELM327Func( void * parameter) {
+  for(;;) {
+    ELM327Manager.readAllData();
+  }
+}
 
-  ELM327Manager.readAllData();
-  oledPrintData();
+void startReadDataFromELM327Async() {
+
+  if (taskLoadingAnimation != NULL) {
+    Serial.println("Task taskReadDataFromELM327 already running? Skipping start...");
+    return;
+  }
+
+  Serial.println("Starting task taskReadDataFromELM327...");
+
+  xTaskCreatePinnedToCore(
+      taskReadDataFromELM327Func, /* Function to implement the task */
+      "taskReadDataFromELM327", /* Name of the task */
+      10000,  /* Stack size in words */
+      NULL,  /* Task input parameter */
+      0,  /* Priority of the task */
+      &taskReadDataFromELM327,  /* Task handle. */
+      0); /* Core where the task should run */
+}
+
+void stopReadDataFromELM327Async() {
+
+  if (taskReadDataFromELM327 != NULL) {
+
+    Serial.println("Stopping task taskReadDataFromELM327...");
+    vTaskDelete(taskReadDataFromELM327);
+    taskReadDataFromELM327 = NULL;
+    Serial.println("Stopped task taskReadDataFromELM327!");
+
+    return;
+  }
+
+  Serial.println("Task taskReadDataFromELM327 already stopped? Skipping stop...");
+}
+
+void taskLoadingAnimationFunc( void * parameter) {
+  for(;;) {
+    displayManager.loadingAnimation();
+  }
+}
+
+void startLoadingAnimationAsync() {
+
+  if (taskLoadingAnimation != NULL) {
+    Serial.println("Task taskLoadingAnimation already running? Skipping start...");
+    return;
+  }
+
+  Serial.println("Starting task taskLoadingAnimation...");
+
+  xTaskCreatePinnedToCore(
+      taskLoadingAnimationFunc, /* Function to implement the task */
+      "taskLoadingAnimation", /* Name of the task */
+      10000,  /* Stack size in words */
+      NULL,  /* Task input parameter */
+      0,  /* Priority of the task */
+      &taskLoadingAnimation,  /* Task handle. */
+      0); /* Core where the task should run */
+}
+
+void stopLoadingAnimationAsync() {
+
+  if (taskLoadingAnimation != NULL) {
+    
+    Serial.println("Stopping task taskLoadingAnimation...");
+    vTaskDelete(taskLoadingAnimation);
+    taskLoadingAnimation = NULL;
+    Serial.println("Stopped task taskLoadingAnimation!");
+    return;
+  }
+
+  Serial.println("Task taskLoadingAnimation already stopped? Skipping stop...");
 }
 
 void setup()
@@ -78,7 +155,8 @@ void loop() {
 
   if (!bluetoothManager.isConnected()) {
 
-    displayManager.loadingAnimation();
+    stopReadDataFromELM327Async();
+    startLoadingAnimationAsync();
 
     // If BT is not connected, forse EML327 init
     ELM327Manager.resetInitState();
@@ -91,7 +169,9 @@ void loop() {
 
     if (ELM327Manager.isInitialized()) {
 
-      readAndPrintAllData();
+      stopLoadingAnimationAsync();
+      startReadDataFromELM327Async();
+      oledPrintData();
     }
   }
 }
