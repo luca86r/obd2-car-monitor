9489dc3039f96b873487aa7856d7dff07c3385d4
Luca Rubin <luca.rubin@intesys.it>
2024-11-15 15:43:07 +0100

    Reduced main loop delay

--- src/config.h
+++ src/config.h
@@ -4,7 +4,7 @@ const char * const OBD_BT_DEVICE_NAME = "vLinker MC-Android";
 const bool DEBUG_MODE = false;
 
 // loop(), delay in ms
-const unsigned long LOOP_DELAY = 400;
+const unsigned long LOOP_DELAY = 300;
 
 // delay between reading data from EML327Manager (in ms)
 const unsigned long READ_ELM327_DATA_DELAY = 0;
